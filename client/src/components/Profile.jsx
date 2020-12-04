b75745306e4a15cd639eeaf749af42b70461370f
webreznov-studio <webreznov.landing@gmail.com>
2020-12-05 00:49:50 +0300

    feat: new test template

---
+++ client/src/components/Profile.jsx
@@ -0,0 +1,202 @@
+import React from 'react';
+
+export default function Home() {
+    return (
+      <div className="container">
+        <Head>
+          <title>webreznov-audio</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Header />
+        <main>
+          <div className="grid">
+            <Link href="/downloading">
+              <a className="card">
+                <h3>Загрузка &rarr;</h3>
+                <p>Загрузить файл на сервер</p>
+              </a>
+            </Link>
+            <a href="https://nextjs.org/learn" className="card">
+              <h3>Как это работает &rarr;</h3>
+              <p>Описание функционала сервиса</p>
+            </a>
+            <a
+              href="https://github.com/vercel/next.js/tree/master/examples"
+              className="card"
+            >
+              <h3>Плеер &rarr;</h3>
+              <p>Слушать музыку</p>
+            </a>
+  
+            <a
+              href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+              className="card"
+            >
+              <h3>Настройки &rarr;</h3>
+              <p>
+                Настройки профиля
+              </p>
+            </a>
+          </div>
+        </main>
+  
+        <footer>
+          <a
+            href="https://webreznov.now.sh"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Разработано от студии{' '}
+            <h2> #webreznov__studio</h2>
+          </a>
+        </footer>
+  
+        <style jsx>{`
+          .container {
+            min-height: 100vh;
+            padding: 0 0.5rem;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+          }
+  
+          main {
+            padding: 5rem 0;
+            flex: 1;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+          }
+  
+          footer {
+            width: 100%;
+            height: 100px;
+            border-top: 1px solid #eaeaea;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          }
+  
+          footer img {
+            margin-left: 0.5rem;
+          }
+  
+          footer a {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          }
+  
+          a {
+            color: inherit;
+            text-decoration: none;
+          }
+  
+          .title a {
+            color: #0070f3;
+            text-decoration: none;
+          }
+  
+          .title a:hover,
+          .title a:focus,
+          .title a:active {
+            text-decoration: underline;
+          }
+  
+          .title {
+            margin: 0;
+            line-height: 1.15;
+            font-size: 4rem;
+          }
+  
+          .title,
+          .description {
+            text-align: center;
+          }
+  
+          .description {
+            line-height: 1.5;
+            font-size: 1.5rem;
+          }
+  
+          code {
+            background: #fafafa;
+            border-radius: 5px;
+            padding: 0.75rem;
+            font-size: 1.1rem;
+            font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+              DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+          }
+  
+          .grid {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex-wrap: wrap;
+  
+            max-width: 800px;
+            margin-top: 3rem;
+          }
+  
+          .card {
+            margin: 1rem;
+            flex-basis: 45%;
+            padding: 1.5rem;
+            text-align: left;
+            color: inherit;
+            text-decoration: none;
+            border: 1px solid #eaeaea;
+            border-radius: 10px;
+            transition: color 0.15s ease, border-color 0.15s ease;
+          }
+  
+          .card:hover,
+          .card:focus,
+          .card:active {
+            color: #0070f3;
+            border-color: #0070f3;
+          }
+  
+          .card h3 {
+            margin: 0 0 1rem 0;
+            font-size: 1.5rem;
+          }
+  
+          .card p {
+            margin: 0;
+            font-size: 1.25rem;
+            line-height: 1.5;
+          }
+  
+          .logo {
+            height: 1em;
+          }
+  
+          @media (max-width: 600px) {
+            .grid {
+              width: 100%;
+              flex-direction: column;
+            }
+          }
+        `}</style>
+  
+        <style jsx global>{`
+          html,
+          body {
+            padding: 0;
+            margin: 0;
+            font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+              Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+              sans-serif;
+          }
+  
+          * {
+            box-sizing: border-box;
+          }
+        `}</style>
+      </div>
+    )
+  }
+  
