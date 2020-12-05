9790881b443f16d22b6ada7542033bf132b6a3ca
webreznov-studio <webreznov.landing@gmail.com>
2020-12-05 11:34:43 +0300

    keks

--- server/db.js
+++ server/db.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "postgres",
-  password: "kthl8822",
-  host: "localhost",
+  user: "yphgufcuilujsd",
+  password: "b2fa413a9f9918e4c3fa1fb59f5d4ccbb9286b65683a00b8e978d627d0388bb8",
+  host: "ec2-54-197-34-207.compute-1.amazonaws.com",
   port: 5432,
-  database: "perntodo"
+  database: "daogsq516giovp"
 });
 
 module.exports = pool;
